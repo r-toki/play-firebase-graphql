37f4015dd0d2ba2af4c86b3552a2908ba37947e7
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-12 23:57:27 +0900

    cloud functions の graphql server を追加

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,6 +1,21 @@
+import { ApolloServer, gql } from "apollo-server-cloud-functions";
 import * as functions from "firebase-functions";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+exports.api = functions.https.onRequest(server.createHandler() as any);
