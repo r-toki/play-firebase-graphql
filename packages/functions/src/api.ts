c4a166aadcb7d8ec86554a8e3c64488aee19be45
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-13 21:35:19 +0900

    graphql server を毎回作らない

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -6,22 +6,22 @@ import { auth, firestore } from "firebase-admin";
 import { typeDefs } from "./graphql/typeDefs";
 import { resolvers } from "./resolvers";
 
-export const getApiApp = async () => {
-  const apiApp = express();
-  apiApp.use(cors());
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({ req }) => {
-      const idToken = req.header("authorization")?.split("Bearer ")[1];
-      if (idToken) {
-        const decodedIdToken = await auth().verifyIdToken(idToken);
-        return { decodedIdToken, db: firestore() };
-      }
-      return { decodedIdToken: undefined, db: firestore() };
-    },
-  });
-  await server.start();
+const apiApp = express();
+apiApp.use(cors());
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    const idToken = req.header("authorization")?.split("Bearer ")[1];
+    if (idToken) {
+      const decodedIdToken = await auth().verifyIdToken(idToken);
+      return { decodedIdToken, db: firestore() };
+    }
+    return { decodedIdToken: undefined, db: firestore() };
+  },
+});
+server.start().then(() => {
   server.applyMiddleware({ app: apiApp });
-  return apiApp;
-};
+});
+
+export { apiApp };
