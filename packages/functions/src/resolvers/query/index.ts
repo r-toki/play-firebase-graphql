3e86a4eba9decedc125d3364f5bded0505a60877
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-26 13:14:15 +0900

    fix

--- packages/functions/src/resolvers/query/index.ts
+++ packages/functions/src/resolvers/query/index.ts
@@ -8,27 +8,29 @@ import { followRelationshipsRef, tweetsRef, usersRef } from "../../lib/typed-ref
 import { UserTweetData } from "../../lib/typed-ref/types";
 
 export const Query: Resolvers["Query"] = {
-  me: (parent, args, { decodedIdToken, db }) => {
-    if (!decodedIdToken) throw new Error("");
-    return getDoc(usersRef(db).doc(decodedIdToken.uid));
+  me: (parent, args, context) => {
+    if (!context.decodedIdToken) throw new Error("");
+    return getDoc(usersRef(context.db).doc(context.decodedIdToken.uid));
   },
-  user: (parent, args, { db }) => getDoc(usersRef(db).doc(args.id)),
-  users: (parent, args, { db }) => getDocs(usersRef(db).orderBy("createdAt", "desc")),
-  tweet: async (parent, args, { db }) => {
-    const docs = await getDocs(tweetsRef(db).where("tweetId", "==", args.id));
+  user: (parent, args, context) => getDoc(usersRef(context.db).doc(args.id)),
+  users: (parent, args, context) => getDocs(usersRef(context.db).orderBy("createdAt", "desc")),
+  tweet: async (parent, args, context) => {
+    const docs = await getDocs(tweetsRef(context.db).where("tweetId", "==", args.id));
     const doc = first(docs);
     if (!doc) throw new Error("");
     return doc;
   },
-  feed: async (parent, args, { decodedIdToken, db }) => {
-    if (!decodedIdToken) throw new Error("");
+  feed: async (parent, args, context) => {
+    if (!context.decodedIdToken) throw new Error("");
 
-    const { uid } = decodedIdToken;
+    const { uid } = context.decodedIdToken;
     const { first, after } = args;
 
-    const relationships = await getDocs(followRelationshipsRef(db).where("followerId", "==", uid));
+    const relationships = await getDocs(
+      followRelationshipsRef(context.db).where("followerId", "==", uid)
+    );
     const queries = [uid, ...relationships.map((v) => v.followedId)].map((id) =>
-      tweetsRef(db).where("creatorId", "==", id).orderBy("createdAt", "desc")
+      tweetsRef(context.db).where("creatorId", "==", id).orderBy("createdAt", "desc")
     );
     const order = (snaps: QueryDocumentSnapshot<UserTweetData>[]) =>
       orderBy(snaps, (snap) => snap.data().createdAt, "desc");
@@ -47,8 +49,8 @@ export const Query: Resolvers["Query"] = {
 
     return { edges: tweetEdges, pageInfo };
   },
-  tweetEdge: async (parent, args, { db }) => {
-    const docs = await getDocs(tweetsRef(db).where("tweetId", "==", args.id));
+  tweetEdge: async (parent, args, context) => {
+    const docs = await getDocs(tweetsRef(context.db).where("tweetId", "==", args.id));
     const doc = first(docs);
     if (!doc) throw new Error("");
     return { node: doc, cursor: doc.createdAt.toDate().toISOString() };
--- packages/functions/src/resolvers/root-types/index.ts
+++ packages/functions/src/resolvers/root-types/index.ts
@@ -4,24 +4,28 @@ import { usersRef, userTweetsRef } from "../../lib/typed-ref";
 import { followRelationshipsRef } from "./../../lib/typed-ref/index";
 
 export const User: Resolvers["User"] = {
-  tweets: (parent, args, { db }) =>
-    getDocs(userTweetsRef(db, { userId: parent.id }).orderBy("createdAt", "desc")),
-  followings: async (parent, args, { db }) => {
+  tweets: (parent, args, context) =>
+    getDocs(userTweetsRef(context.db, { userId: parent.id }).orderBy("createdAt", "desc")),
+  followings: async (parent, args, context) => {
     const relationships = await getDocs(
-      followRelationshipsRef(db).where("followerId", "==", parent.id).orderBy("createdAt", "desc")
+      followRelationshipsRef(context.db)
+        .where("followerId", "==", parent.id)
+        .orderBy("createdAt", "desc")
     );
     const followingsIds = relationships.map((v) => v.followedId);
-    return Promise.all(followingsIds.map((id) => getDoc(usersRef(db).doc(id))));
+    return Promise.all(followingsIds.map((id) => getDoc(usersRef(context.db).doc(id))));
   },
-  followers: async (parent, args, { db }) => {
+  followers: async (parent, args, context) => {
     const relationships = await getDocs(
-      followRelationshipsRef(db).where("followedId", "==", parent.id).orderBy("createdAt", "desc")
+      followRelationshipsRef(context.db)
+        .where("followedId", "==", parent.id)
+        .orderBy("createdAt", "desc")
     );
     const followerIds = relationships.map((v) => v.followedId);
-    return Promise.all(followerIds.map((id) => getDoc(usersRef(db).doc(id))));
+    return Promise.all(followerIds.map((id) => getDoc(usersRef(context.db).doc(id))));
   },
 };
 
 export const Tweet: Resolvers["Tweet"] = {
-  creator: (parent, args, { db }) => getDoc(usersRef(db).doc(parent.creatorId)),
+  creator: (parent, args, context) => getDoc(usersRef(context.db).doc(parent.creatorId)),
 };
