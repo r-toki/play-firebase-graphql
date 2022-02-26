5771933f7017573740447c181e5b36f35ad2fc74
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-26 20:40:06 +0900

    fix

--- packages/client/src/components/index-page/Feed.tsx
+++ packages/client/src/components/index-page/Feed.tsx
@@ -199,7 +199,10 @@ const Tweets: VFC = () => {
 };
 
 const Likes: VFC = () => {
-  const { tweets, hasNext, loading, loadMore } = useFavoriteTweets();
+  const { tweets, hasNext, loading, fetch, loadMore } = useFavoriteTweets();
+  useEffect(() => {
+    fetch();
+  }, []);
 
   return (
     <Stack>
--- packages/client/src/hooks/useFeed.ts
+++ packages/client/src/hooks/useFeed.ts
@@ -5,11 +5,12 @@ import { useCollection } from "react-firebase-hooks/firestore";
 
 import { db } from "../firebase-app";
 import {
-  useFavoriteTweetsForIndexPageQuery,
+  useFavoriteTweetsForIndexPageLazyQuery,
   useFeedForIndexPageQuery,
   useTweetEdgeForIndexPageLazyQuery,
 } from "../graphql/generated";
 import { tweetEventsRef } from "../lib/typed-ref";
+import { useAuthed } from "./../context/Authed";
 
 gql`
   query feedForIndexPage($first: Int!, $after: String) {
@@ -79,7 +80,7 @@ export const useFeed = () => {
 };
 
 export const useFavoriteTweets = () => {
-  const { data, loading, fetchMore } = useFavoriteTweetsForIndexPageQuery({
+  const [fetch, { data, loading, fetchMore }] = useFavoriteTweetsForIndexPageLazyQuery({
     variables: { first: 20 },
     notifyOnNetworkStatusChange: true,
   });
@@ -93,11 +94,13 @@ export const useFavoriteTweets = () => {
     fetchMore({ variables: { first: 10, after: endCursor } });
   };
 
-  return { tweets, hasNext, loading, loadMore };
+  return { tweets, hasNext, loading, fetch, loadMore };
 };
 
 export const useSubscribeTweets = () => {
   const client = useApolloClient();
+
+  const { currentUser } = useAuthed();
   const [getTweetEdge] = useTweetEdgeForIndexPageLazyQuery();
 
   const now = useMemo(() => Timestamp.now(), []);
@@ -107,12 +110,56 @@ export const useSubscribeTweets = () => {
     tweetEvents?.docChanges().forEach(async (change) => {
       if (change.type !== "added") return;
 
-      if (change.doc.data().type === "create" || change.doc.data().type === "update") {
-        console.log("--- tweet has been created/updated ---");
+      if (change.doc.data().type === "create") {
+        console.log("--- tweet has been created ---");
+
+        const tweetEdgeResult = await getTweetEdge({
+          variables: { id: change.doc.data().tweetId },
+        });
+        const tweetEdge = tweetEdgeResult.data?.tweetEdge;
+        if (!tweetEdge) return;
+
+        client.cache.modify({
+          id: client.cache.identify({ __typename: "User", id: currentUser.id }),
+          fields: {
+            feed(existing) {
+              if (!existing) return existing;
+              const pageInfo = existing.pageInfo;
+              if (pageInfo.endCursor > tweetEdge.cursor) return existing;
+              const edges = [tweetEdge, ...existing.edges];
+              return { ...existing, edges };
+            },
+          },
+        });
+      }
+
+      if (change.doc.data().type === "update") {
+        console.log("--- tweet has been updated ---");
+        await getTweetEdge({
+          variables: { id: change.doc.data().tweetId },
+        });
       }
 
       if (change.doc.data().type === "delete") {
         console.log("--- tweet has been deleted ---");
+
+        client.cache.modify({
+          id: client.cache.identify({ __typename: "User", id: currentUser.id }),
+          fields: {
+            feed(existing) {
+              const edges = existing.edges.filter(
+                (v: any) => v.node.__ref !== `Tweet:${change.doc.data().tweetId}`
+              );
+              return { ...existing, edges };
+            },
+            favoriteTweets(existing) {
+              const edges = existing.edges.filter(
+                (v: any) => v.node.__ref !== `Tweet:${change.doc.data().tweetId}`
+              );
+              return { ...existing, edges };
+            },
+          },
+        });
       }
     });
   }, [tweetEvents]);
