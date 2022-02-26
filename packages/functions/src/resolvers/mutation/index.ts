feec423ea3d84ef7c7ec60adb4514f4570bbb29a
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-26 13:56:41 +0900

    fix

--- packages/functions/src/resolvers/mutation/index.ts
+++ packages/functions/src/resolvers/mutation/index.ts
@@ -3,19 +3,24 @@ import { first } from "lodash";
 import { v4 } from "uuid";
 
 import { Resolvers } from "../../graphql/generated";
+import { isSignedIn } from "../../lib/authorization";
 import { getDoc, getDocs } from "../../lib/query/util/get";
 import { followRelationshipsRef, usersRef } from "../../lib/typed-ref";
 import { userTweetsRef } from "./../../lib/typed-ref/index";
 
 export const Mutation: Resolvers["Mutation"] = {
-  updateProfile: async (parent, args, { decodedIdToken, db }) => {
-    if (!decodedIdToken) throw new Error("");
+  updateProfile: async (parent, args, context) => {
+    isSignedIn(context);
 
-    const { uid } = decodedIdToken;
+    const { input } = args;
+    const {
+      decodedIdToken: { uid },
+      db,
+    } = context;
 
     await usersRef(db).doc(uid).set(
       {
-        displayName: args.input.displayName,
+        displayName: input.displayName,
         updatedAt: Timestamp.now(),
       },
       { merge: true }
@@ -24,68 +29,73 @@ export const Mutation: Resolvers["Mutation"] = {
     return getDoc(usersRef(db).doc(uid));
   },
 
-  createTweet: async (parent, args, { decodedIdToken, db }) => {
-    if (!decodedIdToken) throw new Error("");
+  createTweet: async (parent, args, context) => {
+    isSignedIn(context);
 
-    const { uid } = decodedIdToken;
+    const { input } = args;
+    const {
+      decodedIdToken: { uid },
+      db,
+    } = context;
 
     const tweetId = v4();
     await userTweetsRef(db, { userId: uid })
       .doc(tweetId)
       .set({
-        content: args.input.content,
+        content: input.content,
         createdAt: FieldValue.serverTimestamp() as Timestamp,
         updatedAt: FieldValue.serverTimestamp() as Timestamp,
         tweetId,
         creatorId: uid,
       });
+    const tweet = getDoc(userTweetsRef(db, { userId: uid }).doc(tweetId));
 
-    return getDoc(userTweetsRef(db, { userId: uid }).doc(tweetId));
+    return tweet;
   },
 
-  follow: async (parent, args, { decodedIdToken, db }) => {
-    if (!decodedIdToken) throw new Error("");
+  follow: async (parent, args, context) => {
+    isSignedIn(context);
 
-    const { uid } = decodedIdToken;
-    const { followedId } = args.input;
+    const { input } = args;
+    const {
+      decodedIdToken: { uid },
+      db,
+    } = context;
 
     const relationshipDocs = await getDocs(
       followRelationshipsRef(db)
         .where("followerId", "==", uid)
-        .where("followedId", "==", followedId)
+        .where("followedId", "==", input.followedId)
     );
-
     const relationshipDoc = first(relationshipDocs);
-
     if (relationshipDoc) return getDoc(usersRef(db).doc(uid));
-
     await followRelationshipsRef(db).add({
-      followedId,
+      followedId: input.followedId,
       followerId: uid,
       createdAt: Timestamp.now(),
     });
+    const me = getDoc(usersRef(db).doc(uid));
 
-    return getDoc(usersRef(db).doc(uid));
+    return me;
   },
 
-  unFollow: async (parent, args, { decodedIdToken, db }) => {
-    if (!decodedIdToken) throw new Error("");
+  unFollow: async (parent, args, context) => {
+    isSignedIn(context);
 
-    const { uid } = decodedIdToken;
-    const { followedId } = args.input;
+    const { input } = args;
+    const {
+      decodedIdToken: { uid },
+      db,
+    } = context;
 
     const relationshipDocs = await getDocs(
       followRelationshipsRef(db)
         .where("followerId", "==", uid)
-        .where("followedId", "==", followedId)
+        .where("followedId", "==", input.followedId)
     );
-
     const relationshipDoc = first(relationshipDocs);
-
     if (!relationshipDoc) return getDoc(usersRef(db).doc(uid));
-
     await relationshipDoc.ref.delete();
-
     return getDoc(usersRef(db).doc(uid));
   },
 };
