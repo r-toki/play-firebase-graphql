ee6cbd63b657119b734b899b491d40792c6dc134
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-27 18:45:59 +0900

    fix

--- packages/functions/src/lib/repositories/like.ts
+++ packages/functions/src/lib/repositories/like.ts
@@ -1,7 +1,9 @@
 import { Firestore, Timestamp } from "firebase-admin/firestore";
-import { first, last } from "lodash";
+import { first as firstOfList, last, orderBy } from "lodash";
 
+import { Edge, execMultiQueriesWithCursor } from "../query-util/exec-multi-queries-with-cursor";
 import { getDocs } from "../query-util/get";
+import { UserTweetDoc } from "../typed-ref/types";
 import { likesRef, tweetsRef } from "./../typed-ref/index";
 
 // NOTE: Query
@@ -9,25 +11,34 @@ export const getFavoriteTweets = async (
   db: Firestore,
   { userId, first, after }: { userId: string; first: number; after: string | null | undefined }
 ) => {
-  const likeDocs = await getDocs(
-    likesRef(db)
-      .where("userId", "==", userId)
-      .orderBy("createdAt", "desc")
-      .startAfter(after ? Timestamp.fromDate(new Date(after)) : Timestamp.now())
-      .limit(first)
-  );
+  const query = async ({ after }: { after: string }) => {
+    const likeDocs = await getDocs(
+      likesRef(db)
+        .where("userId", "==", userId)
+        .orderBy("createdAt", "desc")
+        .startAfter(Timestamp.fromDate(new Date(after)))
+        .limit(1)
+    );
+    const likeDoc = firstOfList(likeDocs);
+    if (!likeDoc) return [];
 
-  const tweetDocsList = await Promise.all(
-    likeDocs.map((v) => getDocs(tweetsRef(db).where("tweetId", "==", v.tweetId)))
-  );
+    const tweetDocs = await getDocs(tweetsRef(db).where("tweetId", "==", likeDoc.tweetId));
+    const tweetDoc = firstOfList(tweetDocs);
+    if (!tweetDoc) return [];
 
-  const tweetEdges = tweetDocsList.flat().map((doc, idx) => ({
-    node: doc,
-    cursor: likeDocs[idx].createdAt.toDate().toISOString(),
-  }));
-  const pageInfo = { hasNext: tweetEdges.length === first, endCursor: last(tweetEdges)?.cursor };
+    return [{ node: tweetDoc, cursor: likeDoc.createdAt.toDate().toISOString() }];
+  };
 
-  return { edges: tweetEdges, pageInfo };
+  const order = (edges: Edge<UserTweetDoc>[]) => orderBy(edges, (v) => v.cursor, "desc");
+
+  const edges = await execMultiQueriesWithCursor([query], order, {
+    first,
+    after: after ?? new Date().toISOString(),
+  });
+
+  const pageInfo = { hasNext: edges.length === first, endCursor: last(edges)?.cursor };
+
+  return { edges, pageInfo };
 };
 
 export const checkFavorite = async (
@@ -37,7 +48,7 @@ export const checkFavorite = async (
   const likeDocs = await getDocs(
     likesRef(db).where("userId", "==", userId).where("tweetId", "==", tweetId)
   );
-  const likeDoc = first(likeDocs);
+  const likeDoc = firstOfList(likeDocs);
   return likeDoc ? true : false;
 };
 
@@ -49,7 +60,7 @@ export const like = async (
   const likeDocs = await getDocs(
     likesRef(db).where("userId", "==", userId).where("tweetId", "==", tweetId)
   );
-  const likeDoc = first(likeDocs);
+  const likeDoc = firstOfList(likeDocs);
   if (likeDoc) throw new Error("likeDoc not found");
   const ref = await likesRef(db).add({ userId, tweetId, createdAt: Timestamp.now() });
   const addedLikeDoc = (await ref.get()).data();
@@ -64,7 +75,7 @@ export const unLike = async (
   const likeDocs = await getDocs(
     likesRef(db).where("userId", "==", userId).where("tweetId", "==", tweetId)
   );
-  const likeDoc = first(likeDocs);
+  const likeDoc = firstOfList(likeDocs);
   if (!likeDoc) throw new Error("likeDoc not found");
   await likeDoc.ref.delete();
   return likeDoc;
