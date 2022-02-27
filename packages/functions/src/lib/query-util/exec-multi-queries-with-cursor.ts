b42f2c70fd3dde2380b1174f03b297f828b75c8c
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-27 21:00:33 +0900

    fix

--- packages/functions/src/lib/query-util/exec-multi-queries-with-cursor.ts
+++ packages/functions/src/lib/query-util/exec-multi-queries-with-cursor.ts
@@ -1,16 +1,16 @@
-import { first as firstOfList } from "lodash";
+import { first as firstOfList, uniqBy } from "lodash";
 
 export type Edge<T> = {
   node: T;
   cursor: string;
 };
 
-export const execMultiQueriesWithCursor = async <T>(
+export const execMultiQueriesWithCursor = async <T extends { id: string }>(
   queries: (({ after }: { after: string }) => Promise<Edge<T>[]>)[],
   order: (edges: Edge<T>[]) => Edge<T>[],
   { first, after }: { first: number; after: string }
 ): Promise<Edge<T>[]> => {
-  const res: Edge<T>[] = [];
+  let res: Edge<T>[] = [];
 
   const candidatesList = await Promise.all(queries.map((query) => query({ after })));
 
@@ -18,7 +18,7 @@ export const execMultiQueriesWithCursor = async <T>(
   while (i < first) {
     const head = firstOfList(order(candidatesList.flat()));
     if (!head) return res;
-    res.push(head);
+    res = uniqBy([...res, head], (v) => v.node.id);
     const headIndex = candidatesList.findIndex((edges) => firstOfList(edges) === head);
     if (headIndex < 0 || queries.length < headIndex) throw new Error("not found headIndex");
     const headQuery = queries[headIndex];
