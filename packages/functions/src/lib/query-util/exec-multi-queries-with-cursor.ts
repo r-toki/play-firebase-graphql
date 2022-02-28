6b3483baff16aedce6e759b4a0952f3d5b22960a
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-28 21:22:21 +0900

    fix

--- packages/functions/src/lib/query-util/exec-multi-queries-with-cursor.ts
+++ packages/functions/src/lib/query-util/exec-multi-queries-with-cursor.ts
@@ -1,4 +1,4 @@
-import { first as firstOfList, uniqBy } from "lodash";
+import { first as head, uniqBy } from "lodash";
 
 export type Edge<T> = {
   node: T;
@@ -16,14 +16,18 @@ export const execMultiQueriesWithCursor = async <T extends { id: string }>(
 
   let i = 0;
   while (i < first) {
-    const head = firstOfList(order(candidatesList.flat()));
-    if (!head) return res;
-    res = uniqBy([...res, head], (v) => v.node.id);
-    const headIndex = candidatesList.findIndex((edges) => firstOfList(edges) === head);
+    const headCandidate = head(order(candidatesList.flat()));
+    if (!headCandidate) return res;
+
+    res = uniqBy([...res, headCandidate], (v) => v.node.id);
+
+    const headIndex = candidatesList.findIndex((edges) => head(edges) === headCandidate);
     if (headIndex < 0 || queries.length < headIndex) throw new Error("not found headIndex");
+
     const headQuery = queries[headIndex];
-    const newCandidates = await headQuery({ after: head.cursor });
+    const newCandidates = await headQuery({ after: headCandidate.cursor });
     candidatesList[headIndex] = newCandidates;
+
     i++;
   }
 
